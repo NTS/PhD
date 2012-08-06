ea14d178fd635aa730c1d070ee30e8e402eb628b
Max F. Albrecht <1@178.is>
2012-08-06 22:43:54 +0200

    autocommit at 2012-08-06T20:43:54+00:00 (UTC) by mfa on 13.lan
    IT_WORKS

--- .papermill/message.sh
+++ .papermill/message.sh
@@ -4,3 +4,5 @@
 
 ### if on OS X, ask iTunes for the current playing track
 [[ $(uname) == "Darwin" ]] && osascript -e 'tell application "System Events" to if ((name of processes) contains "iTunes") then do shell script ("osascript -e " & quoted form of ("tell application \"iTunes\" to if player state is playing then \"iTunes is playing: \" & name of current track & \" - \" & artist of current track" & ""))'
+
+echo IT_WORKS
