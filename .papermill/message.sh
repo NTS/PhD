11aaf264d24b551ff151845f74438ae5250b9a4e
Naomi T. Salmon <nts@nts.is>
2012-08-07 15:25:49 +0200

    autocommit at 2012-08-07T13:25:49+00:00 (UTC) by nts on elf.local

--- .papermill/message.sh
+++ .papermill/message.sh
@@ -5,7 +5,6 @@
 # Get the local time
 echo "Local Time: $(date)"
 
-
 ### if on OS X, ask iTunes for the current playing track
 [[ $(uname) == "Darwin" ]] && osascript -e 'tell application "System Events" to if ((name of processes) contains "iTunes") then do shell script ("osascript -e " & quoted form of ("tell application \"iTunes\" to if player state is playing then \"iTunes is playing: \" & name of current track & \" - \" & artist of current track" & ""))'
 
