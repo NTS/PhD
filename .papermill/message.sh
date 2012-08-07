d0e88680c926ed47e43794e61574d7025e18b0fb
Max F. Albrecht <1@178.is>
2012-08-07 13:22:31 +0200

    optimize message script
    only ask external tools once for data.
    
    time before:
        real	0m2.104s
        user	0m0.811s
        sys	0m0.529s
    
    time after:
        real	0m1.304s
        user	0m0.348s
        sys	0m0.336s
    
    Output before:
    Local Time: Di  7 Aug 2012 13:10:46 CEST
    Wifi SSID: ALi23
    Temperature LEFT PALM REST: 33 C
    User Name: Max F. Albrecht (mfa)
    System Version: OS X 10.8 (12A269)
    Time since boot: 58 minutes
    Model Name: MacBook Pro
    Model Identifier: MacBookPro5,5
    Model Name: MacBook Pro
    AC Charger Connected: Yes
    Fully Charged: Yes
    Charging: No
    Charge Remaining (mAh): 3757
    
    Output after:
    Local Time: Di  7 Aug 2012 13:20:21 CEST
    Wifi SSID: ALi23
    Temperature LEFT PALM REST: 33 C
    User Name: Max F. Albrecht (mfa)
    System Version: OS X 10.8 (12A269)
    Time since boot: 1:08
    Model Name: MacBook Pro
    Model Identifier: MacBookPro5,5
    Model Name: MacBook Pro
    AC Charger Connected: Yes
    Fully Charged: Yes
    Charging: No
    Charge Remaining (mAh): 3757
    
    Signed-off-by: Max F. Albrecht <1@178.is>

--- .papermill/message.sh
+++ .papermill/message.sh
@@ -10,29 +10,31 @@ echo "Local Time: $(date)"
 [[ $(uname) == "Darwin" ]] && osascript -e 'tell application "System Events" to if ((name of processes) contains "iTunes") then do shell script ("osascript -e " & quoted form of ("tell application \"iTunes\" to if player state is playing then \"iTunes is playing: \" & name of current track & \" - \" & artist of current track" & ""))'
 
 ### if on OS X, get the current Wifi SSID #fixme
-[[ $(uname) == "Darwin" ]] && \
-AIRPORT_STATUS=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)
+[[ $(uname) == "Darwin" ]] && AIRPORT_STATUS=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)
 echo "$AIRPORT_STATUS" | grep "AirPort: Off">/dev/null && WIFI_STATUS="Off"
 [[ ! $WIFI_STATUS == "Off" ]] && WIFI_STATUS=$(echo "$AIRPORT_STATUS" | awk -F': ' '/ SSID/ {print $2}') && echo "Wifi SSID: "$WIFI_STATUS""
 #
 
 ### if TempMonitor.app is installed, get the current Temperature
-MAC_TEMPERATURE_TOOL="/Applications/TemperatureMonitor.app/Contents/MacOS/tempmonitor"
-[[ -f "$MAC_TEMPERATURE_TOOL" ]] && echo Temperature "$($MAC_TEMPERATURE_TOOL -a -l -c | grep "SMC LEFT PALM REST: " | cut -c 5-99)"
+[[ $(uname) == "Darwin" ]] &&MAC_TEMPERATURE_TOOL="/Applications/TemperatureMonitor.app/Contents/MacOS/tempmonitor"
+[[ $(uname) == "Darwin" ]] && [[ -f "$MAC_TEMPERATURE_TOOL" ]] && echo Temperature "$($MAC_TEMPERATURE_TOOL -a -l -c | grep "SMC LEFT PALM REST: " | cut -c 5-99)"
 
 ### if on OS X, get Software info from 'system_profiler' #fixme
-[[ $(uname) == "Darwin" ]] && system_profiler SPSoftwareDataType | grep "User Name: " | cut -c 7-99
-[[ $(uname) == "Darwin" ]] && system_profiler SPSoftwareDataType | grep "System Version: " | cut -c 7-99
-[[ $(uname) == "Darwin" ]] && system_profiler SPSoftwareDataType | grep "Time since boot: " | cut -c 7-99
+[[ $(uname) == "Darwin" ]] && SYSPROF_SOFTW=$(system_profiler SPSoftwareDataType)
+[[ $(uname) == "Darwin" ]] && echo "$SYSPROF_SOFTW" | grep "User Name: " | cut -c 7-99
+[[ $(uname) == "Darwin" ]] && echo "$SYSPROF_SOFTW" | grep "System Version: " | cut -c 7-99
+[[ $(uname) == "Darwin" ]] && echo "$SYSPROF_SOFTW" | grep "Time since boot: " | cut -c 7-99
 
 ### if on OS X, get Hardware and Power info from 'system_profiler' #fixme
-[[ $(uname) == "Darwin" ]] && system_profiler SPHardwareDataType | grep "Model Name: " | cut -c 7-99
-[[ $(uname) == "Darwin" ]] && system_profiler SPHardwareDataType | grep "Model Identifier: " | cut -c 7-99
-[[ $(uname) == "Darwin" ]] && system_profiler SPHardwareDataType | grep "Model Name: " | cut -c 7-99
-[[ $(uname) == "Darwin" ]] && echo "AC Charger $(system_profiler SPPowerDataType | grep "Connected: Yes" | cut -c 7-99)"
-[[ $(uname) == "Darwin" ]] && system_profiler SPPowerDataType | grep "Fully Charged: " | cut -c 11-99
-[[ $(uname) == "Darwin" ]] && system_profiler SPPowerDataType | grep "Charging: " | head -1 | cut -c 11-99
-[[ $(uname) == "Darwin" ]] && system_profiler SPPowerDataType | grep "Charge Remaining (mAh): " | cut -c 11-99
+[[ $(uname) == "Darwin" ]] && SYSPROF_HARDW=$(system_profiler SPHardwareDataType)
+[[ $(uname) == "Darwin" ]] && echo "$SYSPROF_HARDW" | grep "Model Name: " | cut -c 7-99
+[[ $(uname) == "Darwin" ]] && echo "$SYSPROF_HARDW" | grep "Model Identifier: " | cut -c 7-99
+[[ $(uname) == "Darwin" ]] && echo "$SYSPROF_HARDW" | grep "Model Name: " | cut -c 7-99
+[[ $(uname) == "Darwin" ]] && SYSPROF_POWER=$(system_profiler SPPowerDataType)
+[[ $(uname) == "Darwin" ]] && echo "AC Charger $(echo "$SYSPROF_POWER" | grep "Connected: Yes" | cut -c 7-99)"
+[[ $(uname) == "Darwin" ]] && echo "$SYSPROF_POWER" | grep "Fully Charged: " | cut -c 11-99
+[[ $(uname) == "Darwin" ]] && echo "$SYSPROF_POWER" | grep "Charging: " | head -1 | cut -c 11-99
+[[ $(uname) == "Darwin" ]] && echo "$SYSPROF_POWER" | grep "Charge Remaining (mAh): " | cut -c 11-99
 
 ### if on OS X, get active network info from 'system_profiler' #fixme
 #[[ $(uname) == "Darwin" ]] && echo "Network: "
