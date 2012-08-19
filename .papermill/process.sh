02b5cd1a3708226b1b781a634c4e787d457ec979
Max F. Albrecht <1@178.is>
2012-08-19 20:22:06 +0200

    migrate to pandoc for conversion to HTML
    Signed-off-by: Max F. Albrecht <1@178.is>

--- .papermill/process.sh
+++ .papermill/process.sh
@@ -11,8 +11,8 @@ do
     echo "[>] Entering Folder "$DIR"..."
     cd "$BASE_DIR"/"$DIR"
 
-    # Process MARKDOWN Files
-    echo "[#] Processing MARKDOWN..."
+    # Process CHAPTER Files
+    echo "    [#] Processing CHAPTERS..."
 
     # delete $TMP_FILE if exists
     TMP_FILE=".$DIR.generated.markdown.tmp"
@@ -22,15 +22,33 @@ do
     for FILE in *.markdown
     do
         [[ -f "$FILE" ]] || continue ## abort if no match in DIR
-        echo "    [+] $FILE"
+        echo "        [+] $FILE"
         cat "$FILE" >> "$TMP_FILE"
         echo "\n" >> "$TMP_FILE"
     done
     
     # Move the generated markdown file into base dir
-    [[ -f "$TMP_FILE" ]] && mv "$TMP_FILE" ../"$DIR.generated.markdown" && echo "[!] Generated "$DIR.generated.markdown" in "$BASE_DIR"."
+    [[ -f "$TMP_FILE" ]] && mv "$TMP_FILE" ../"$DIR.generated.markdown" && echo "    [!] Generated "$DIR.generated.markdown" in "$BASE_DIR"."
 
-    # END Process MARKDOWN Files
+    # END Pre-process CHAPTER Files
+
+    # Process MARKDOWN Files
+	cd "$BASE_DIR"
+	pwd
+    echo "    [#] Processing MARKDOWN..."
+
+	pandoc "$DIR.generated.markdown" \
+	--write=html5 --standalone --smart --toc --section-divs --normalize --number-sections \
+	--css=".papermill/swiss.css"
+	--bibliography="./BIBTEX/biblio.phd.nts.bib" \
+	--csl=".papermill/CSL/din-1505-2.csl" \
+	--output="$DIR.generated.html"
+	
+	echo "[<] Finished processing "$DIR"..."
+	# END Process MARKDOWN Files
+	
+	# open -g "$DIR.generated.html"
 
 done
-echo "[<] Finished processing "$DIR"..."
+
+open -g "PhD_Dissertation.generated.html"
